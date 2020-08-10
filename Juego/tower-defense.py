a5fca29375bf9d329e844117a1262f19754a1cd5
SantiSinesi <61692221+SantiSinesi@users.noreply.github.com>
2020-08-10 09:04:47 -0300

    Update tower-defense.py

--- Juego/tower-defense.py
+++ Juego/tower-defense.py
@@ -8,6 +8,8 @@ pantalla = pygame.display.set_mode([400, 400])
 
 NEGRO = (0, 0, 0)
 BLANCO = (255, 255, 255)
+VERDE = (0, 255, 0)
+ROJO = (255, 0, 0)
 
 imagen_de_fondo = pygame.image.load("fondoprehistoria.png").convert()
 defensor = pygame.image.load("wachinprehistoria.jpg").convert()
